882c86e2c295a2fce33588abff77dfbc7913dcd2
David <davidsoft97@gmail.com>
2025-09-16 22:54:05 -0600

    Se agrego una función para suma

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string texto="Hello, World!";
 Console.WriteLine($"{texto}");
+
+
+void sumar()
+{
+    Console.WriteLine("Sumando...");
+}
